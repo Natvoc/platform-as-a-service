8f87e8a83454f10a70ca79f7afffe799b7d8bd75
Natyari Vargas <naryari@gmail.com>
2022-06-13 23:43:04 -0600

    Add files via upload

--- sites.py
+++ sites.py
@@ -15,3 +15,20 @@ import pulumi.automation as auto
 from pulumi_aws import s3
 
 bp = Blueprint("sites", __name__, url_prefix="/sites")
+
+
+def create_pulumi_program(content: str):
+    # Create a bucket and exponse a website index document
+    site_bucket = s3.Bucket(
+        "s3-website-bucket", website=s3.BucketWebsiteArgs(index_document="index.html")
+    )
+    index_content = content
+
+    # Write our index.html into the site bucket
+    s3.BucketObject(
+        "index",
+        bucket=site_bucket.id,
+        conent=index_content,
+        key="index.html",
+        content_type="text/html"; charset=utf-8,
+        )
